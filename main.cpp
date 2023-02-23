196e960446f18b97abdc72d342388f449b2e81e5
Anna Babayan <annababayanru@gmail.com>
2023-02-23 17:23:45 +0300

    upd_file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    string filename = argv[0];   // Name of the file
+    ifstream newfile (filename);
+    newfile.open(filename);
+    if (newfile.is_open()){ //checking whether the file is open
+        cout << "File is open";
+        string line;
+        while (newfile >> line) {
+            cout << line << endl;
+        }
+        newfile.close(); //close the file object.
+    } else {
+        cout << "File isn't open";
+    }
+
+    return 0;
+
 }
